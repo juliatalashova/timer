3d1c279c47e92b9cb3d881916c065168648eff73
Yuliia <julia.gancheva1990@gmail.com>
2020-05-18 17:27:58 +0300

    Master - Pomodoro timer created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+import { FontAwesome } from '@fortawesome/react-fontawesome'
 function App() {
+  let [counter, setCounter] = useState(3000)
+  const [paused, setPaused] = useState(false);
+
+  useEffect(() => {
+    if(paused) {
+      const timer = setInterval(() => {
+        setCounter(counter => Math.max(counter - 100, 0))
+      }, 100);
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [paused]);
+
+  function startPauseTimer() {
+    setPaused(counter ? !paused : false)
+  }
+  function resetTimer() {
+    setPaused(false);
+    setCounter(3000)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pomodoro timer</h1>
+      <div className="content">
+        <div className="time">
+          Seconds Left:
+          <span>{(counter / 1000).toFixed(1)}</span>
+        </div>
+        <div className="run">
+          Running:
+          <span>{paused ? "true" : "false"}</span>
+        </div>
+      </div>
+      <div className="btns-container">
+        <button type="button" className="btn-play" disabled={!counter} onClick={startPauseTimer}>
+          {paused ? <i className="fa fa-pause fa-2x"></i> : <i className="fa fa-play fa-2x"></i>}
+        </button>
+        <button type="button" className="btn-stop" onClick={resetTimer}>
+          <i className="fa fa-stop fa-2x"></i>
+        </button>
+      </div>
     </div>
   );
 }
